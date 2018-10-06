faa5e0a81e27433e41adc3e8b0317d1155b7ca7f
hare451g <hendrasadewa30@gmail.com>
2018-10-06 22:45:57 +0700

    add created at and updated at field to todo list

--- src/services/todo.services.js
+++ src/services/todo.services.js
@@ -1,15 +1,27 @@
 const todoModel = require('../model/todo.model');
-
 const status = require('../utils/constants');
+const moment = require('moment');
 
 const createNewTodo = (req, res) => {
-    const query = new todoModel(req.body.todo);
-    query.save((err, query) => {
-        if (err) {
-            res.status(status.HTTP_STATUS.ERROR).json(err);
+    try {
+        const newTodo = {
+            name: req.body.todo.name,
+            completed: false,
+            created_at: moment(),
+            updated_at: moment(),
         }
-        res.status(status.HTTP_STATUS.SUCCESS).json(query);
-    })
+    
+        const query = new todoModel(newTodo);
+    
+        query.save((err, query) => {
+            if (err) {
+                res.status(status.HTTP_STATUS.ERROR).json(err);
+            }
+            res.status(status.HTTP_STATUS.SUCCESS).json(query);
+        });
+    } catch (error) {
+        res.status(status.HTTP_STATUS.ERROR).json({messsage: 'malformed request', error: `${error}`});
+    }
 };
 
 const fetchAllTodo = (req, res) => {
@@ -17,7 +29,10 @@ const fetchAllTodo = (req, res) => {
         if (err) {
             res.status(status.HTTP_STATUS.ERROR).json(err);
         }
-        res.status(status.HTTP_STATUS.SUCCESS).json(result)
+        if (result === null) {
+            res.status(status.HTTP_STATUS.SUCCESS).json();
+        }
+        res.status(status.HTTP_STATUS.SUCCESS).json(result);
     });
 };
 
@@ -27,20 +42,35 @@ const fetchOneTodo = (req, res) => {
         if (err) {
             res.status(status.HTTP_STATUS.ERROR).json(err);
         }
-        res.status(status.HTTP_STATUS.SUCCESS).json(result);
-    })
-}
-
-const updateTodo = (req, res) => {
-    const id = req.params.id;
-    const updatedData = req.body.todo;
-    todoModel.findByIdAndUpdate(id, updatedData, (err, result) => {
-        if (err) {
-            res.status(status.HTTP_STATUS.ERROR).json(err);
+        if (result === null) {
+            res.status(status.HTTP_STATUS.NOT_FOUND).json({ message: 'not found', id});
         }
         res.status(status.HTTP_STATUS.SUCCESS).json(result);
-    })
-}
+    });
+};
+
+const updateTodo = (req, res) => {
+    try {
+        const id = req.params.id;
+        todoModel.findById(id, (err, result) => {
+            if (err) {
+                res.status(status.HTTP_STATUS.NOT_FOUND).json({ message: 'Not found !'});
+            }
+            result.name = req.body.todo.name || result.name,
+            result.completed = req.body.todo.completed || result.name,
+            result.updated_at = moment(),
+            
+            result.save((err, updated) => {
+                if (err) {
+                    res.status(status.HTTP_STATUS.ERROR).json(err);
+                }
+                res.status(status.HTTP_STATUS.SUCCESS).json(updated);      
+            })
+        })
+    } catch (error) {
+        res.status(status.HTTP_STATUS.ERROR).json({messsage: 'malformed request', error: `${error}`});
+    }
+};
 
 const deleteTodo = (req, res) => {
     const id = req.params.id;
@@ -49,8 +79,8 @@ const deleteTodo = (req, res) => {
             res.status(status.HTTP_STATUS.ERROR).json(err);
         }
         res.status(status.HTTP_STATUS.SUCCESS).json(result);
-    })
-}
+    });
+};
 
 module.exports = {
     createNewTodo,
